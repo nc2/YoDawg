b74f857d9b3133f26bb70a416d7b5157a0425860
JD Courtoy <jd.courtoy@gmail.com>
2015-04-11 09:31:32 -0500

    Updates to the sample scaffolding application.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var path = require('path');
+var _ = require('lodash');
 var yeoman = require('yeoman-generator');
+var path = require('path');
+var mkdirp = require('mkdirp');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var eol = require('os').EOL;
@@ -12,29 +14,90 @@ var Generator = yeoman.generators.Base.extend({
   constructor: function () {
     yeoman.generators.Base.apply(this, arguments);
 
-    this.argument('appName', {
-      type: String,
-      required: false
-    });
+    this.argument('name', { type: String, required: false });
+    this.appName = utils.lowerCamelName(this.name);
+    this.appDir = 'app';
   },
 
   info: function () {
     this.log(yosay(
       chalk.red('Yo Dawg!') + eol +
-      chalk.yellow('Freaking', chalk.bold('_Awesome_ ')) +
-      chalk.yellow('Angular scaffolds of scaffolds ') + eol +
-      chalk.gray('Black Bar Labs  ')
+      chalk.yellow('Freaking', chalk.bold('_Awesome_ '), 'Angular scaffolds of scaffolds ') + eol +
+      chalk.gray('Black Bar Labs  ') // NOTE: extra spaces necessary to deal with bugs in yosay
     ));
   },
 
-  composeScaffold: function () {
-    var args = this.appName ? [ this.appName ] : [];
-    this.composeWith('gulp-angular', {
-      args: args,
-      options: {
-        'skip-welcome-message': 'true'
+  checkConfig: function () {
+    if (!this.config.get('config')) {
+      return;
+    }
+
+    var done = this.async();
+    this.prompt([{
+      type: 'confirm',
+      name: 'existingConfig',
+      message: 'Yo, there\'s an existing .yo-rc configuration!  Would you like to use it?',
+      default: true
+    }], function (answers) {
+      this.existingConfig = answers.existingConfig
+      done();
+    }.bind(this));
+  },
+
+  projectPrompts: function () {
+    if (this.existingConfig) {
+      return;
+    }
+
+    var done = this.async();
+    this.prompt([{
+      when: function (answers) { return !this.appname; }.bind(this),
+      type: 'input',
+      name: 'name',
+      message: 'Yo, what would you like me to name this app?',
+      default: utils.lowerCamelName(path.basename(process.cwd()))
+    }], function (answers) {
+      if (answers.name) {
+        this.appName = utils.lowerCamelName(answers.name);
       }
-    });
+
+      done();
+    }.bind(this));
+  },
+
+  clientPrompts: function () {
+    // None, yet
+  },
+
+  displayName: function () {
+    this.log('Creating ' + chalk.green(this.appName) + ' app.');
+  },
+
+  projectFiles: function () {
+    this.copy('__editorconfig', '.editorconfig');
+    this.copy('__jshintrc', '.jshintrc');
+    this.copy('__jscsrc', '.jscsrc');
+    this.copy('__bowerrc', '.bowerrc');
+  },
+
+  packageFiles: function () {
+    this.copy('gulp.png', 'gulp.png');
+    this.copy('_package.json', 'package.json');
+    this.template('_bower.json', 'bower.json');
+    this.template('_gulpfile.js', 'gulpfile.js');
+    this.template('_gulp.config.js', 'gulp.config.js');
+    this.template('_karma.conf.js', 'karma.conf.js');
+    this.template('_README.md', 'README.md');
+  },
+
+  appFiles: function () {
+    this.directory('assets/images');
+    this.directory('assets/styles');
+
+    this.directory('app', this.appDir);
+
+    this.template('_index.html', 'index.html');
+    this.template('_specs.html', 'specs.html');
   },
 
   install: function () {
@@ -42,16 +105,19 @@ var Generator = yeoman.generators.Base.extend({
   },
 
   end: function () {
-    // TODO: Pass in configuration values such as the app directory
-    var config = utils.getGeneratorConfig(this.config);
+    // If user chooses to use exsiting yo-rc file, then skip prompts
+    if (this.existingConfig) {
+      return;
+    }
 
-    // Update configurations
-    var rootDir = this.destinationRoot();
     this.config.set({
-      dirs: {
-        app: path.join(config.paths.src, 'app')
+      config: {
+        name: this.appName,
+        paths: {
+          app: this.appDir
+        }
       }
-    })
+    });
   }
 });
 
