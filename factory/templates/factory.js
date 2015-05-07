fa49bbded028601aa497316e2471f897e83ee09d
Tim Goyer <tgoyer@nc2media.com>
2015-05-07 15:05:08 -0500

    Updated factory spec.

--- factory/templates/factory.js
+++ factory/templates/factory.js
@@ -1,30 +1,30 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  /**
-   * @ngdoc service
-   * @name <%= module %>.factory:<%= utils.upperName(name) %>
-   *
-   * @description
-   *
-   */
-  angular
-    .module('<%= module %>')
-    .factory('<%= utils.lowerCamelName(name) %>', <%= utils.lowerCamelName(name) %>);
+    /**
+    * @ngdoc service
+    * @name <%= module %>.factory:<%= utils.upperName(name) %>
+    *
+    * @description
+    *
+    */
+    angular
+        .module('<%= module %>')
+        .factory('<%= utils.lowerCamelName(name) %>', <%= utils.lowerCamelName(name) %>);
 
-  /* @ngInject */
-  function <%= utils.lowerCamelName(name) %> () {
-    var someValue = '<%= utils.upperName(name) %>';
-    var service = {
-      someValue: someValue,
-      someMethod: someMethod
-    };
-    return service;
+    /* @ngInject */
+    function <%= utils.lowerCamelName(name) %> () {
+        var someValue = '<%= utils.upperName(name) %>';
+        var factory = {
+            someValue: someValue,
+            someMethod: someMethod
+        };
+        return factory;
 
-    ///////////
+        ///////////
 
-    function someMethod () {
-      return '<%= utils.lowerCamelName(name) %>';
-    };
-  }
+        function someMethod () {
+            return '<%= utils.lowerCamelName(name) %>';
+        };
+    }
 }());
--- factory/templates/factory.spec.js
+++ factory/templates/factory.spec.js
@@ -2,21 +2,21 @@
     'use strict';
 
     /*global describe, beforeEach, it, expect, inject, module*/
-    describe('<%= utils.lowerCamelName(name) %>', function () {
-      var factory;
+    describe('<%= utils.rootName(module) %>', function () {
+        var factory;
 
-      beforeEach(module('<%= module %>'));
+        describe('Factory: <%= utils.upperName(name) %>', function () {
+            beforeEach(module('<%= module %>'));
+            beforeEach(setup);
 
-      beforeEach(inject(function (<%= utils.lowerCamelName(name) %>) {
-        factory = <%= utils.lowerCamelName(name) %>;
-      }));
+            it('should have someValue be equal to <%= utils.upperName(name) %>', function () {
+                expect(factory.someValue).to.equal('<%= utils.upperName(name) %>');
+            });
+        });
 
-      it('should have someValue be <%= utils.upperName(name) %>', function () {
-        expect(factory.someValue).toEqual('<%= utils.upperName(name) %>');
-      });
-
-      it('should have someMethod return <%= utils.lowerCamelName(name) %>', function () {
-        expect(factory.someMethod()).toEqual('<%= utils.lowerCamelName(name) %>');
-      });
+        function setup() {
+            bard.inject(this, '<%= utils.lowerCamelName(name) %>');
+            factory = <%= utils.lowerCamelName(name) %>;
+        }
     });
 }());
